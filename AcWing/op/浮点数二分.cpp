2c8e13aac252295e10b9bcc89be10e08159073e1
poboll <caiths@icloud.com>
2023-02-10 23:11:03 +0800

    提交项目文件。

---
+++ AcWing/op/浮点数二分.cpp
@@ -0,0 +1,17 @@
+/*浮点数二分*/
+#include<iostream>
+using namespace std;
+int main() {
+  double x;
+  cin >> x;
+  double l = 0, r = x;
+  //不用精度表示（精度足够小就停止）
+  //for (int i = 0; i < 100; i ++)开一百次方-->极小
+  while (r - l >1e-8) {//保留四位小数-1e-6 永远多2 一直做
+    double  mid = (l + r) / 2;//每次求中点
+    if (mid * mid >= x) r = mid;
+    else l = mid;
+  }
+  printf("%lf\n",l);
+  return 0;
+}
