26e1a59fb0a35948eaf9850ec90f6b98057aa4db
poboll <caiths@icloud.com>
2023-02-24 22:11:43 +0800

    提交项目文件。

--- AcWing/op/835.Tire字符串统计.cpp
+++ AcWing/op/835.Tire字符串统计.cpp
@@ -1,4 +1,5 @@
-/*维护一个字符串集合，支持两种操作：
+/*835.Tire字符串统计
+维护一个字符串集合，支持两种操作：
 I x 向集合中插入一个字符串 x；
 Q x 询问一个字符串在集合中出现了多少次。
 共有 N 个操作，输入的字符串总长度不超过 105，字符串仅包含小写英文字母。
@@ -9,7 +10,7 @@ Q x 询问一个字符串在集合中出现了多少次。
 对于每个询问指令 Q x，都要输出一个整数作为结果，表示 x 在集合中出现的次数。
 每个结果占一行。
 数据范围
-1≤N≤2∗104
+1≤N≤2∗10^4
 输入样例：
 5
 I abc
@@ -43,8 +44,7 @@ void insertStr(char str[]) {
 }
 int query(char str[]) {
     int p = 0;
-    for(int i= 0; str[i]; i++)
-    {
+    for(int i= 0; str[i]; i++) {
         int next_idx = str[i] - 'a';//求下一个字符所在的结点的索引
         if(!son[p][next_idx]) return 0;//如果当前字符不存在，则直接返回
         p = son[p][next_idx];//更新下一个字符所在节点的索引
@@ -53,8 +53,8 @@ int query(char str[]) {
 }
 int main() {
     int n;
-    cin>>n;
-    while(n--) {
+    cin >> n;
+    while( n -- ) {
         char op[2], str[N];
         scanf("%s%s", op, str);
         if(op[0] == 'I')  insertStr(str);
