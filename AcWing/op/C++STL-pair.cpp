7006c921333d8888b1597ddb41cc8c9d93ab5a7f
poboll <caiths@icloud.com>
2023-02-28 15:49:29 +0800

    提交项目文件。

---
+++ AcWing/op/C++STL-pair.cpp
@@ -0,0 +1,16 @@
+/*C++STL-pair*/
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+    //初始化
+    pair<int, string>p;
+    p = make_pair(10, "yxc");
+    p = {20, "abc"};
+    //三种属性
+    pair<int, pair<int, int>>q;
+    return 0;
+}
---
+++ AcWing/op/C++STL-概览.cpp
@@ -0,0 +1,13 @@
+/*
+vector, 变长数组, 倍增的思想
+    size() 返回数组元素个数
+    empty() 是否为空
+    clear() 清空
+string, 字符串, substr(), c_str()
+queue, 队列, push(), front(), top(), pop()
+priority_queue, 优先队列, push(), top(), pop()
+stack, 栈, push(), top(), pop()
+deque, 双端队列
+set, map, multiset, multimap, 基于平衡二叉树（红黑树），动态维护有序序列
+unordered_set, unordered_map, unordered_multise, unordered_multimap, 哈希表
+bitset, 压位*/
