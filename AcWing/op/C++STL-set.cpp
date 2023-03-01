3b8b9c6b0a887dc9d62c4d843becfe2f03536d2e
poboll <caiths@icloud.com>
2023-03-01 08:21:47 +0800

    提交项目文件。

---
+++ AcWing/op/C++STL-set.cpp
@@ -0,0 +1,11 @@
+/*set
+    set/multiset//set不能重复元素：忽略
+        insert()  插入一个数
+        find()  查找一个数，不存在返回end迭代器
+        count()  返回某一个数的个数
+        erase()
+            (1) 输入是一个数x，删除所有x   O(k + logn)
+            (2) 输入一个迭代器，删除这个迭代器
+        lower_bound()/upper_bound()
+            lower_bound(x)  返回大于等于x的最小的数的迭代器
+            upper_bound(x)  返回大于x的最小的数的迭代器*/
