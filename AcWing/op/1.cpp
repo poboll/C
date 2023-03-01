7fb0d65109e7564f0326fdb10193730ed82b3df2
poboll <caiths@icloud.com>
2023-03-02 06:42:54 +0800

    提交项目文件。

--- AcWing/op/1.cpp
+++ AcWing/op/1.cpp
@@ -2,14 +2,11 @@
 #include <math.h>
 double sum(double x1, double y1, double x2, double y2);
 int main() {
-    string a = "yxc";
-    a += "def";
-    a += 'C';
-    cout << a << endl;
-    //常用函数
-    cout << a.substr(1,) << endl;
+    double x1, y1, x2, y2;
+    scanf("%ld")
     return 0;
 }
 double sum(double x1, double y1, double x2, double y2) {
-    double temp = sqrt(pow(fabs(x2 - x1), 2))
+    double temp = sqrt(pow(fabs(x2 - x1), 2) + pow(fabs(y2 - y1), 2)) 
+    return temp;
 }
