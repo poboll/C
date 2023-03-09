7fe7e6261dd93e290364297e347d158776e37aa1
poboll <caiths@icloud.com>
2023-03-09 12:28:23 +0800

    提交项目文件。

--- AcWing/op/850.Dijkstra求最短路II-邻接矩阵.cpp
+++ AcWing/op/850.Dijkstra求最短路II-邻接矩阵.cpp
@@ -5,50 +5,36 @@
 using namespace std;
 const int N = 5500;
 typedef pair<int, int> PII; // {距离, 节点编号}
-
 int g[N][N]; // 邻接矩阵
 int dist[N], st[N];
 int n, m;
-
-int dijkstra()
-{
+int dijkstra() {
     memset(dist, 0x3f, sizeof dist);
     dist[1] = 0;
-    
     priority_queue<PII, vector<PII>, greater<PII>> heap; // 小根堆
-    heap.push({0, 1});
-    
-    while (heap.size())
-    {
+    heap.push({0, 1}); 
+    while (heap.size()) {
         PII t = heap.top();
         heap.pop();
-        
         int ver = t.second; // 距离源点最近的点
-        if (st[ver]) continue;
-        
+        if (st[ver])    continue;
         // 利用 t 更新它的邻点
         st[ver] = true; 
-        for (int i = 1; i <= n; i++)
-        {
+        for (int i = 1; i <= n; i ++) {
             // 比较 1->i 和 1->ver->i
-            if (dist[i] > dist[ver] + g[ver][i])
-            {
+            if (dist[i] > dist[ver] + g[ver][i]) {
                 dist[i] = dist[ver] + g[ver][i];
                 heap.push({dist[i], i});
             }
         }
     }
-    
-    if (dist[n] == 0x3f3f3f3f) return -1;
+    if (dist[n] == 0x3f3f3f3f)  return -1;
     else return dist[n];
 }
-
-int main()
-{
+int main() {
     memset(g, 0x3f, sizeof g);
     cin >> n >> m;
-    while (m --)
-    {
+    while (m -- ) {
         int a, b, c;
         cin >> a >> b >> c;
         g[a][b] = min(g[a][b], c); // 处理重边
--- AcWing/op/850.Dijkstra求最短路II-邻接表.cpp
+++ AcWing/op/850.Dijkstra求最短路II-邻接表.cpp
@@ -24,61 +24,43 @@
 using namespace std;
 const int N = 1e6 + 10;
 typedef pair<int, int> PII; // 堆里存储 { 距离, 节点编号 }
-
 int h[N], w[N], e[N], ne[N], idx; // 邻接表（稀疏图）
 int dist[N]; // 距离源点的距离
 bool st[N]; // 是否已经找到最短路
 int n, m;
-
 // 添加 a 指向 b 边长为 c 的边
-void add(int a, int b, int c)
-{
+void add(int a, int b, int c) {
     e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx ++ ;
 }
-
-int dijkstra()
-{
+int dijkstra() {
     memset(dist, 0x3f, sizeof dist); // 距离初始化为无穷大
     dist[1] = 0;
-    
     priority_queue<PII, vector<PII>, greater<PII>> heap; // 小根堆
     heap.push({0, 1}); // 1 号点距离源点距离为 1
-
-    while (heap.size())
-    {
+    while (heap.size()) {
         PII t = heap.top(); // 距离源点最近的点
         heap.pop();
-
         int ver = t.second; // 节点编号
         // int distance = t.first; // 源点距离 ver 的距离
-
         if (st[ver]) continue; // 如果距离已经确定，则跳过该点
-        
         // 更新 ver 所指向的节点距离       
         st[ver] = true;
-        for (int i = h[ver]; i != -1; i = ne[i])
-        {
+        for (int i = h[ver]; i != -1; i = ne[i]) {
             int j = e[i];
 			// 比较 1-->j 和 1-->ver-->j 的距离
-            if (dist[j] > dist[ver] + w[i])
-            {
+            if (dist[j] > dist[ver] + w[i]) {
                 dist[j] = dist[ver] + w[i];
                 heap.push({dist[j], j}); // 距离变小，则入堆
             }
         }
     }
-
-    if (dist[n] == 0x3f3f3f3f) return -1;
+    if (dist[n] == 0x3f3f3f3f)  return -1;
     return dist[n];
 }
-
-int main()
-{
+int main() {
     memset(h, -1, sizeof h);
-
     cin >> n >> m;
-    while (m -- )
-    {
+    while (m -- ) {
         int a, b, c;
         cin >> a >> b >> c;
         add(a, b, c); // 邻接表不需要考虑重边
