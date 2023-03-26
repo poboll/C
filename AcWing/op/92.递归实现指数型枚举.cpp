d32a2f9b76062b66bdd6e6e4d1d43a209c573f7c
poboll <caiths@icloud.com>
2023-03-26 23:34:08 +0800

    提交项目文件。

--- AcWing/op/92.递归实现指数型枚举.cpp
+++ AcWing/op/92.递归实现指数型枚举.cpp
@@ -3,3 +3,18 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+const int N = 15;
+int n;
+int st[N];//状态，记录每个位置当前的状态：0表示还没有考虑，1表示选它，2表示不选它
+void dfs(int u) {
+    if(u == n) {
+        for(int i = 0; i < n; i ++)
+        return;
+    }
+}
+int main() {
+    cin >> n;
+    dfs(0);//全局数组 省略
+    return 0;
+    
+}
