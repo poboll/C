c211adcf25f078bbc225cd37fd348e130155b7cd
poboll <caiths@icloud.com>
2023-03-27 13:44:01 +0800

    提交项目文件。

--- AcWing/op/92.递归实现指数型枚举.cpp
+++ AcWing/op/92.递归实现指数型枚举.cpp
@@ -6,12 +6,14 @@ using namespace std;
 const int N = 15;
 int n;
 int st[N];//状态，记录每个位置当前的状态：0表示还没有考虑，1表示选它，2表示不选它
+int ways[1 << 15][15], cnt;
 void dfs(int u) {
     if(u == n) {
-        for(int i = 0; i < n; i ++)
+        //记录方案
+        for(int i = 1; i <= n; i ++)
             if(st[i] == 1)
-                printf("%d ", i);
-        printf("\n");
+                ways[cnt][i] = i;
+        cnt ++;
         return;
     }
     st[u] = 2;
@@ -19,10 +21,14 @@ void dfs(int u) {
     st[u] = 0;//恢复现场
     st[u] = 1;
     dfs(u + 1);//第二个分支：选
-    st[u] = 0;
+    st[u] = 0;//恢复现场
 }
 int main() {
     cin >> n;
     dfs(1);//全局数组 省略
+    for(int i = 0; i < cnt; i ++) {
+        for(int j = 1; j < n; j ++) printf("%d ", ways[i][j]);
+        puts("");//换行
+    }
     return 0;
 }
