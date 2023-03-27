36987719e4aa3c24de280c19955f0430fd41a588
poboll <caiths@icloud.com>
2023-03-27 14:05:20 +0800

    提交项目文件。

--- AcWing/op/92.递归实现指数型枚举-vector.cpp
+++ AcWing/op/92.递归实现指数型枚举-vector.cpp
@@ -1,35 +1,35 @@
 #include <cstdio>
-#include <cstring>
 #include <iostream>
+#include <cstring>
 #include <algorithm>
 #include <vector>
 using namespace std;
-const int N = 15;
+const int N = 16;
 int n;
-int st[N];//状态，记录每个位置当前的状态：0表示还没有考虑，1表示选它，2表示不选它
-vector<vector<int>> ways;
-void dfs(int u) {
-    if(u == n) {
-        //记录方案
-        for(int i = 1; i <= n; i ++)
-            if(st[i] == 1)
-                way.push_back(i);
-        ways.push_back(way);
+int st[N];//状态，记录每个位置当前的状态，0，未考虑，1选，2不选  
+vector<vector<int>> ways;   //二维数组
+void dfs(int u) {//u表示当前我们正在做第几位
+    if(u > n) {//边界
+        vector<int> way;
+        for(int i = 1; i <= n;i ++) //记录方案
+            if(st[i] == 1)          //当前位置为1，表示我们选了，输出
+                way.push_back(i);       
+        ways.push_back(way);    // 把way方案存到ways里面去
         return;
     }
-    st[u] = 2;
-    dfs(u + 1);//第一个分支：不选
-    st[u] = 0;//恢复现场
+    st[u] = 2; 
+    dfs(u + 1); //第一个分支，不选
+    st[u] = 0;  //恢复现场
     st[u] = 1;
-    dfs(u + 1);//第二个分支：选
-    st[u] = 0;//恢复现场
+    dfs(u + 1); //第二个分支，选
+    st[u] = 0;
 }
 int main() {
     cin >> n;
-    dfs(1);//全局数组 省略
-    for(int i = 0; i < ways.size(); i ++) {
-        for(int j = 0; j < ways[i].size(); j ++) printf("%d ", ways[i][j]);
-        puts("");//换行
+    dfs(1);//st数组是全局的，不用传进去
+    for (int i = 0; i < ways.size(); i ++){
+        for (int j = 0; j < ways[i].size(); j ++ ) printf("%d ",ways[i][j]);
+        puts("");//输出一个空字符串+换行
     }
     return 0;
 }
