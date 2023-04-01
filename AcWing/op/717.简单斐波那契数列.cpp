ea859383e1674f121e764f9c8aa31ea9ed36b1f1
poboll <caiths@icloud.com>
2023-04-01 11:54:10 +0800

    提交项目文件。

--- AcWing/op/717.简单斐波那契数列.cpp
+++ AcWing/op/717.简单斐波那契数列.cpp
@@ -13,3 +13,19 @@
 输出样例：
 0 1 1 2 3
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a=0,b=1;
+    for(int i=0;i<n;i++)
+    {
+        cout<<a<<' ';
+        int c=a+b;
+        a=b,b=c;
+    }
+    cout<<endl;
+    return 0;
+}
