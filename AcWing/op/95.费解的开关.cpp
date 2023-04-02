7d57e37c3e3ae2d0078bc42c0b18c05b1447f1d4
poboll <caiths@icloud.com>
2023-04-02 17:22:10 +0800

    提交项目文件。

--- AcWing/op/95.费解的开关.cpp
+++ AcWing/op/95.费解的开关.cpp
@@ -64,3 +64,89 @@
 点击的先后顺序不影响最终结果。
 于是，我们不妨先考虑第一行如何点击。在枚举第一行的点击方案后，就可以认为第一行固定不动，在考虑2 ~ 5行如何点击。而按照上述性质2，此时第2 ~ 5行的点击方案是确定的。
 若到达第n行是不全为1，说明这种点击方式不合法。在所有合法的点击方式中取点击次数最少的就是答案。*/
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int N = 510;
+int n;
+char g[N][N] , backup[N][N]; //backup是备份数组
+
+void turn(int x , int y)
+{
+    int dx[5] = {0 , 1 , 0 , -1 , 0} , dy[5] = {1 , 0 , -1 , 0 , 0};
+    for(int i = 0; i < 5; i++)
+    {
+        int a = x + dx[i] , b = y + dy[i];
+        if(a < 0 || a >= 5 || b < 0 || b >= 5) continue;
+        // 0的ASCII码为48  1的ASCII码为49 所以只需要对'0' ^ 1 即可得到 '1' 相反同理
+        g[a][b] ^= 1;
+    }
+}
+
+int main()
+{
+    cin >> n;
+    
+    while(n--)
+    {
+        
+        for(int i = 0; i < 5; i++) cin >> g[i];
+        
+        int res = 10; // 记录结果
+        
+        // 枚举第一行所有的按开关或不按开关的状态，1:按开关  0:不按开关
+        for(int i = 0; i < 32; i++)       
+        {
+            int step = 0; //记录步数
+            
+            // 先将当前数组备份到backup数组中,因为每次遍历第一行，要使用原始数组来遍历
+            memcpy(backup , g , sizeof g);  
+            
+            // 看第一行的第j列是否需要按开关
+            for(int j = 0; j < 5; j++)
+            {
+                if(i >> j & 1) // 如果第j位是1的话,就需要按开关,否则不按开关
+                {
+                    step++; // 记录步数
+                    turn(0 , j); // 按开关
+                }
+            }
+            
+            // 遍历其他行，由于其他行是否按开关都只与上一行有关，所以只遍历0~4行即可
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if(g[i][j] == '0') // 如果第i行j类是关灯状态，那么就必须按第i+1行j列的开关
+                    {
+                        step++;
+                        turn(i + 1 , j);
+                    }
+                }
+            }
+            
+            // 最后只需要看最后一行是否有灯关着即可
+            bool dark = false;
+            for(int i = 0; i < 5; i++)
+            {
+                if(g[4][i] == '0')
+                {
+                    dark = true;
+                    break;
+                }
+            }
+            
+            // 如果灯全部打开
+            if(!dark) res = min(res , step);
+            
+            memcpy(g , backup , sizeof g); // 将原始数组重新赋值回去，供第一行遍历
+        }
+        
+        if(res > 6) res = -1;
+        cout << res << endl;
+    }
+    
+    return 0;
+}
