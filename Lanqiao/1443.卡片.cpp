5973bdf4bf4359a3ed2792967359814aa3475a9a
poboll <caiths@icloud.com>
2023-04-05 13:03:16 +0800

    提交项目文件。

--- Lanqiao/1443.卡片.cpp
+++ Lanqiao/1443.卡片.cpp
@@ -12,14 +12,14 @@
   最大运行内存: 128M*/
 #include <stdio.h>
 int main(void) {
-	int i,t,sum=0;
-	for(i=1;;i++) {
-		for(t=i;t!=0;t/=10)
-			if(t%10==1)
-				sum++;
-		if(sum>=2021)
+	int i, t, sum = 0;
+	for(i = 1; ; i ++) {
+		for(t = i; t != 0; t /= 10)
+			if(t % 10 == 1)
+				sum ++;
+		if(sum >= 2021)
 			goto end;
 	}
-	end:printf("%d",i);
+	end:printf("%d", i);
 	return 0;
 }
