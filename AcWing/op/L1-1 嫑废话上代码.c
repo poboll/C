10cd3193c82a936acbd3b5fa78e96aaf64608b8b
poboll <caiths@icloud.com>
2023-04-16 13:51:29 +0800

    提交项目文件。

---
+++ AcWing/op/L1-1 嫑废话上代码.c
@@ -0,0 +1,16 @@
+/*L1-1 嫑废话上代码
+Linux 之父 Linus Torvalds 的名言是：“Talk is cheap. Show me the code.”（嫑废话，上代码）。本题就请你直接在屏幕上输出这句话。
+输入格式：
+本题没有输入。
+输出格式：
+在一行中输出 Talk is cheap. Show me the code.。
+输入样例：
+无
+输出样例：
+Talk is cheap. Show me the code.
+代码长度限制
+16 KB
+时间限制
+400 ms
+内存限制
+64 MB*/
