01668c90ba8e238fc353642acfb0da9ea5f8dc50
poboll <caiths@icloud.com>
2023-05-30 12:51:17 +0800

    提交项目文件

--- c.c
+++ c.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <stdbool.h>
 
 
 int main()
@@ -8,7 +9,19 @@ int main()
     printf("吴粤大麻瓜\n");
     mySqrt(1);
     maximum()
+    sumNums（）
     return 0;
+    isPowerOfThree()
+    isPowerOfTwo();
+    isPowerOfFour()
+    kthFactor(
+            isPerfectSquare(
+    int i, sum = 0;             // (2)
+    for(i = 1; i <= n;) {       // (3)
+        sum += i;               // (4)
+        ++i;
+    }
+
 }
 
 返回类型 函数名 (参数列表)
@@ -38,4 +51,104 @@ int mySqrt(int x) {                     //(1)
 
 int maximum(int a, int b){
     return a > b ? a : b;   //(1)
+}
+
+int sumNums(int n){             // (1)
+    int i;                      // (2)
+    int sum = 0;                // (3)
+    for(i = 1; i <= n; ++i) {   // (4)
+        sum += i;               // (5)
+    }
+    return sum;                 // (6)
+}
+
+bool isPowerOfTwo(int n);
+
+bool isPowerOfTwo(int n) {
+    int i;
+    unsigned int k = 1;         // (1)
+    if(n <= 0) {
+        return false;           // (2)
+    }
+    if(n == 1) {
+        return true;            // (3)
+    }
+    for(i = 1; i <= 31; ++i) {
+        k *= 2;                 // (4)
+        if(k == n) {
+            return true;        // (5)
+        }
+    }
+    return false;
+}
+
+bool isPowerOfThree(int n);
+
+
+bool isPowerOfThree(int n) {
+    int i;
+    unsigned int k = 1;
+    if(n <= 0) {
+        return false;
+    }
+    if(n == 1) {
+        return true;
+    }
+    for(i = 1; i <= 31; ++i) {  // (1)
+        k *= 3;                 // (2)
+        if(k == n) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isPowerOfFour(int n);
+
+
+bool isPowerOfFour(int n) {
+    int i;
+    unsigned int k = 1;
+    if(n <= 0) {
+        return false;
+    }
+    if(n == 1) {
+        return true;
+    }
+    for(i = 1; i <= 15; ++i) {  // (1)
+        k *= 4;                 // (2)
+        if(k == n) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int kthFactor(int n, int k){
+    int i;
+    int sum = 0;                // (1)
+    for(i = 1; i <= n; ++i) {   // (2)
+        if(n % i -- 0) {        // (3)
+            ++cnt;
+            if(cnt == k) {
+                return i;       // (4)
+            }
+        }
+    }
+    return -1;                  // (5)
+}
+
+int isPerfectSquare(int X){
+    int i;
+    long long p;
+    for(i = 1;; ++i) {      // (1)
+        p = (long long)i*i; // (2)
+        if(p == x) {
+            return true;    // (3)
+        }
+        if(p > x) {
+            return false;   // (4)
+        }
+    }
+    return false;           // (5)
 }
