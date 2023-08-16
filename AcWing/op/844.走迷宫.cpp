ddd91cbbe66fe2117b6800583bb3556c5717cebe
poboll <caiths@icloud.com>
2023-08-16 22:58:40 +0800

    提交项目文件。

--- AcWing/op/844.走迷宫.cpp
+++ AcWing/op/844.走迷宫.cpp
@@ -56,7 +56,23 @@ void bfs(int a, int b) {
 	cout << f[n][m];
 }
 //typedef pair<int, int> PII;
-//const int N =110;
+//const int N = 110;
+//int n, m;
+//int g[N][N];//地图
+//int d[N][N];//路径
+//PII q[N * N],prev[N][N];
+int bfs() {
+    int hh = 0, tt = 0;
+    q[0] = {0, 0};
+    memset(d, -1, sizeof d);//初始化
+    d[0][0] = 0;
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, -1, 0, -1};
+    while(hh <= tt) {
+        auto t = q[hh ++];
+
+    }
+}
 //
 int main() {
     //g数组全部初始化为1，不用判断越界，走不出去了
