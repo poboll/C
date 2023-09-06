2ca7eafb5c8fb8e2bee20ee861f7a45ee781d9d2
poboll <caiths@icloud.com>
2023-09-06 11:08:15 +0800

    提交项目文件。

--- AcWing/op/806.两个数的和.cpp
+++ AcWing/op/806.两个数的和.cpp
@@ -6,11 +6,21 @@
 样例输出
 3.33
 */
-#include <cstdio>
+#include<cstdio>
+#include<iostream>
 
 using namespace std;
 
+double add(double x, double y)
+{
+    return x + y;
+}
+
 int main()
 {
+    double x, y;
+    cin >> x >> y;
+    printf("%.2lf\n", add(x, y));
 
+    return 0;
 }
