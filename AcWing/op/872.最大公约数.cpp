b672b01dc952b2ad97d269088971c7ae478e51c8
poboll <caiths@icloud.com>
2023-09-14 13:32:42 +0800

    提交项目文件。

---
+++ AcWing/op/872.最大公约数.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 大妖怪 on 2023/9/14.
+//
+#include <iostream>
+using namespace std;
+int gcd(int a,int b) {
+    return b ? gcd(b, a % d) : a;//(a, 0)-->a。0可整除任何数
+}
+int main(){
+    int n;
+    while(n--) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        printf("%d\n", gcd(a, b));
+    }
+    return 0;
+}
