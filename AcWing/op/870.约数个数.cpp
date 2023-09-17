c474cd8b8868754933e1644b60b18cb6b446b9ec
poboll <caiths@icloud.com>
2023-09-17 11:31:04 +0800

    提交项目文件。

--- AcWing/op/870.约数个数.cpp
+++ AcWing/op/870.约数个数.cpp
@@ -1,3 +1,36 @@
 //
 // Created by 大妖怪 on 2023/9/13.
 //
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+typedef long long LL;
+
+const int MOD = 1e9 + 7;
+
+int main()
+{
+    int n, x;
+    unordered_map<int, int> primes;
+    cin >> n;
+    while(n --)
+    {
+        cin >> x;
+
+        for(int i = 2; i <= x/i; ++i)
+        {
+            while(x % i == 0)
+            {
+                primes[i]++;
+                x /= i;
+            }
+        }
+        if(x > 1) primes[x] ++;
+    }
+    LL res = 1;
+    for(auto t: primes)
+        res = (res * (t.second+1)) % MOD;
+    cout << res << endl;
+    return 0;
+}
