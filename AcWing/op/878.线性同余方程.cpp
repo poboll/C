30437a5c859c4d0b82879b4f034900c933a0abba
poboll <caiths@icloud.com>
2023-09-26 21:50:40 +0800

    提交项目文件。

--- AcWing/op/878.线性同余方程.cpp
+++ AcWing/op/878.线性同余方程.cpp
@@ -1,3 +1,9 @@
 //
 // Created by 大妖怪 on 2023/9/18.
 //
+#include <iostream>
+using namespace std;
+
+int main () {
+
+}
--- AcWing/op/885.高斯消元解线性方程.cpp
+++ AcWing/op/885.高斯消元解线性方程.cpp
@@ -1,3 +1,8 @@
 //
 // Created by 大妖怪 on 2023/9/19.
 //
+#include <iostream>
+using namespace std;
+int mian () {
+
+}
