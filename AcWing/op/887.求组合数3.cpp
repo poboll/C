4c6c7513f319e7ff43a7c154e552843be43ea81a
poboll <caiths@icloud.com>
2023-10-10 21:51:05 +0800

    提交项目文件。

--- AcWing/op/887.求组合数3.cpp
+++ AcWing/op/887.求组合数3.cpp
@@ -2,8 +2,15 @@
 // Created by 大妖怪 on 2023/9/20.
 //
 #include <iostream>
-using namespace std;
 
 int main() {
+    int sum = 0;
 
+    for (int i = 3; i < 100; i += 3) {
+        sum += i;
+    }
+
+    std::cout << "100以内的3倍数之和为: " << sum << std::endl;
+
+    return 0;
 }
